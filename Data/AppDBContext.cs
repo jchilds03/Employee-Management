a7397c1c2767fa1478f44f43d6a40a741c918a45
jchilds03 <107134781+jchilds03@users.noreply.github.com>
2025-04-15 16:38:01 -0500

    Add files via upload

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Data
+{
+	public class AppDBContext : DbContext
+	{
+		public DbSet<Employee> Employees { get; set; }
+
+		public AppDBContext(DbContextOptions<AppDBContext> options)
+			: base(options)
+		{ }
+	}
+}
+
