848ae0e9bb057fc0f3db824b3ad2e4465ebe9fe4
jchilds03 <107134781+jchilds03@users.noreply.github.com>
2025-04-15 16:41:00 -0500

    Add files via upload

---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EmployeeManagement.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDBContext _context;
+
+        public EmployeeRepository(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployeeAsync(int id)
+        {
+            var employeeInDB = await _context.Employees.FindAsync(id);
+
+            if(employeeInDB == null)
+            {
+                throw new KeyNotFoundException($"Employee with id {id} was not found");
+            }
+
+            _context.Employees.Remove(employeeInDB);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee?> GetByIdAsync(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task UpdateEmplooyeeAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ Repositories/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+	public interface IEmployeeRepository
+	{
+		//Read Functions
+		Task<IEnumerable<Employee>> GetAllAsync();
+		Task<Employee?> GetByIdAsync(int id);
+
+		//Update Function
+		Task AddEmployeeAsync(Employee employee);
+		Task UpdateEmplooyeeAsync(Employee employee);
+
+		//Delete Function
+		Task DeleteEmployeeAsync(int id);
+	}
+}
+
